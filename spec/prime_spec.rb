c79557f2e44960d87b429e4f672bc7bc4466ec79
Antarr Byrd <antarr@chaione.com>
2016-11-27 12:50:06 -0600

    Add spec for take(0)

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe Prime do
-  context 'take' do
+  context 'take 10' do
     subject { Prime.take(10) }
     it 'returns the first to prime numbers' do
       expect(subject).to eq([2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
+    end
+  end
+
+  context 'take 0' do
+    subject { Prime.take(0) }
+    it 'returns an empty array' do
+      expect(subject).to eq([])
     end
   end
 end
