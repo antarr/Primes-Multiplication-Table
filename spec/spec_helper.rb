e3da775652751ac57f95a73b69ea300891da9ac4
Antarr Byrd <antarr@chaione.com>
2016-11-27 17:19:43 -0600

    Add SimpleCov to Test environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/prime'
 require_relative '../lib/prime/table'
+require 'simplecov'
+
+SimpleCov.start
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
